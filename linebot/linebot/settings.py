aeb4840d9526149a6144e66b268fd556bcb1d57b
HiroashiShimada <hiroshi385@shimadahiroshinoMacBook-puro.local>
2018-10-10 03:35:05 +0900

    update

--- linebot/linebot/settings.py
+++ linebot/linebot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's8v!+8o-j1i4=9^so08go!x3n1+3ru^=s!c&&d*2q#qn1^r)5n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','https://floating-scrubland-74825.herokuapp.com']
 
 
 # Application definition
@@ -116,5 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
